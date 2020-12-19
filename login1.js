e1e9ac445fdc8cf641302fa2ca329f119bdd3294
pavithrathampi <pavithrathampib@gmail.com>
2020-12-19 14:05:07 +0530

    case study

---
+++ login1.js
@@ -0,0 +1,16 @@
+
+
+function login(){
+var username= document.getElementById("username");
+var password =document.getElementById("pws");
+
+if((username.value == "admin" )&&( password.value == "12345")){
+ return true;
+ }
+else{
+    alert("invalid username or password!");
+ return false;
+
+}
+}
+
